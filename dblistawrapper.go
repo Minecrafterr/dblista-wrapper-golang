98adfc1ec28053d7dabf85fc1b5610b610a506f8
Minecrafterr <61909942+Minecrafterr@users.noreply.github.com>
2020-07-02 20:51:24 +0200

    Dodano dwie funkcje
    GetBotInfo(id) - id bota trza wpisać
    UpdateStats(token, users, servers) - Token api, liczba użytkowników, liczba serwerów

--- dblistawrapper.go
+++ dblistawrapper.go
@@ -1,7 +1,48 @@
 package dblistawrapper
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"github.com/tidwall/gjson"
+	"bytes"
+)
 
-func Hello() {
-	fmt.Println("Hello, World!")
+func GetBotInfo(id string) {
+	resp, err := http.Get("https://api.dblista.pl/v1/bots/"+id)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	fmt.Println("Name: " + gjson.Get(string(body), "data.name").String() + "\nRating average: " + gjson.Get(string(body), "data.rating.average").String()+"\nVotes count: "+gjson.Get(string(body), "data.votes").String()+"\nYour bot is boosted by user with id "+gjson.Get(string(body), "data.premium.booster").String()+"\nBoost ends at (timestamp) "+gjson.Get(string(body), "data.premium.end").String())
+	}
+
+func UpdateStats(token string, users int, servers int) {
+	client := &http.Client{}
+    
+        body := []byte("{\n  \"members\": users,\n  \"servers\": servers\n}")
+    
+        req, _ := http.NewRequest("POST", "https://api.dblista.pl/v1/bots/stats", bytes.NewBuffer(body))
+    
+        req.Header.Add("Content-Type", "application/json")
+        req.Header.Add("Authorization", token)
+    
+        resp, err := client.Do(req)
+    
+        if err != nil {
+            fmt.Println("Errored when sending request to the server")
+            return
+        }
+    
+        defer resp.Body.Close()
+        resp_body, _ := ioutil.ReadAll(resp.Body)
+    
+        fmt.Println(resp.Status)
+        fmt.Println(string(resp_body))
+}
 }

190cc6db8eb51bc453d5ca67ee6a7bcb3a747328
Minecrafterr <61909942+Minecrafterr@users.noreply.github.com>
2020-07-03 22:34:51 +0200

    Aktualizacja wrappera
    usunięto funkcję GetBotInfoFull ponieważ można przez GetBotInfo od teraz i zwraca d i można se yyy podpiąć czy coś i dawać np. api.data.name

--- dblistawrapper.go
+++ dblistawrapper.go
@@ -4,35 +4,89 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
-	"github.com/tidwall/gjson"
 	"bytes"
 )
 
-func GetBotInfoFull(id string, info string) {
-	resp, err := http.Get("https://api.dblista.pl/v1/bots/"+id)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	gjson.Get(string(body), "data."+info).String()
-}
 
 func GetBotInfo(id string) {
 	resp, err := http.Get("https://api.dblista.pl/v1/bots/"+id)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	
-	fmt.Println("Name: " + gjson.Get(string(body), "data.name").String() + "\nRating average: " + gjson.Get(string(body), "data.rating.average").String()+"\nVotes count: "+gjson.Get(string(body), "data.votes").String()+"\nYour bot is boosted by user with id "+gjson.Get(string(body), "data.premium.booster").String()+"\nBoost ends at (timestamp) "+gjson.Get(string(body), "data.premium.end").String())
+if err != nil {
+    // handle err
+    fmt.Println(err)
+}
+defer resp.Body.Close()
+body, err := ioutil.ReadAll(resp.Body)
+if err != nil {
+    fmt.Println(err)
+}
+
+type VotelogModel struct {
+    Timestamp int `json:"timestamp"`
+    User string `json:"user"`
+}
+type RatingsModel struct {
+    Author string `json:"author"`
+    Rating int `json:"rating"`
+    Details string `json:"details"`
+}
+type InfoModel struct {
+    FullDescription string `json:"fullDescription"`
+    Library string `json:"library"`
+    Prefix string `json:"prefix"`
+    ShortDescription string `json:"shortDescription"`
+    Tags []string `json:"tags"`
+}
+type LinksModel struct {
+    Discord string `json:"discordServer"`
+    Git string `json:"gitRepository"`
+    Www string `json:"website"`
+}
+type PremiumModel struct {
+    Booster string `json:"booster"`
+    CardColor string `json:"cardStyle"`
+    End string `json:"end"`
+    Start string `json:"start"`
+}
+type RatingModel struct {
+    Avarage int `json:"average"`
+    Order int `json:"order"`
+    Ratings []RatingsModel `json:"ratings"`
+}
+type StatModel struct {
+    Members int `json:"members"`
+    MonthlyInvites int `json:"monthlyInvites"`
+    Servers int `json:"servers"`
+    Status string `json:"status"`
+}
+type StatusModel struct {
+    Verification string `json:"verification"`
+}
+type UptimeModel struct {
+    Max int `json:"max"`
+    Online int `json:"online"`
+}
+type BotModel struct {
+    Avatar string `json:"avatarURL"`
+    ID string  `json:"id"`
+    Name string  `json:"name"`
+    Owner string  `json:"ownerID"`
+    Votes uint16  `json:"votes"`
+    Owners []string  `json:"owners"`
+    InvitePermissions int `json:"invitePermissions"`
+    Votelog []VotelogModel `json:"votelog"`
+    Info InfoModel `json:"info"`
+    Links LinksModel `json:"links`
+    Ratings RatingModel `json:"rating"`
+    Stats StatModel `json:"stats"`
+    Status StatusModel `json:"status"`
+    Uptime UptimeModel `json:"uptime"`
+}
+type BotRes struct {
+    Data BotModel `json:"data"`
+  }
+var d BotRes
+json.Unmarshal(body, &d)
+return d
 	}
 
 
